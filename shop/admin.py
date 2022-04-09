b7a48215bfbafc949d7c7533c0d18395a0ce6508
switchboard123 <103297173+switchboard123@users.noreply.github.com>
2022-04-09 17:12:23 +0530

    Delete shop directory

--- shop/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Product,Contact, Orders, OrderUpdate
-
-admin.site.register(Product)
-admin.site.register(Contact)
-admin.site.register(Orders)
-admin.site.register(OrderUpdate)
